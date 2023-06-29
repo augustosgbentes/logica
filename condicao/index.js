2c3dbdb9868b464e73dfe706a27b6e00f38a443f
augustosgbentes <augustobentes@hotmail.com>
2023-06-29 19:52:11 -0300

    aprendendo logica

---
+++ condicao/index.js
@@ -0,0 +1,48 @@
+const pessoa = 'Augusto'
+let idade = 22
+
+
+if (idade > 18) {
+   console.log('verdade', idade)
+}
+else {
+    console.log('falso', idade)    
+}
+
+// Operadores Lógicos
+// Soma: +
+// Subtrair: -
+// Dividir: /
+// Multiplicar: *
+// Resto: %
+
+let num1 = 3
+let num2 = 6
+
+const resultado = num1 + num2
+
+console.log(resultado % 2 === 0) 
+
+if (num1 % 2 === 0) {
+    console.log ('par', num1)
+}
+else {
+    console.log ('impar', num1)
+}
+
+
+// Escreva um programa que verifique se um número é positivo, negativo ou zero.
+
+let Numero = 0;
+
+if (Numero > 0) {
+    console.log('positivo', Numero)
+} 
+else if(Numero === 0) {
+    console.log('zero',Numero)
+}
+else {
+    console.log('negativo', Numero)
+ }
+   
+ 
